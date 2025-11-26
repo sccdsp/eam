e093dacd066bcae3b6a2394dfe4246cd6f93e8c3
SP-LEGION\CDSP1 <cdsp1010@outlook.com>
2025-11-26 17:50:02 +0800

    增加资产录入页面

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Host
+from .forms import HostForm
 # Create your views here.
 def index(request):
     host_list = Host.objects.all()
     return render(request, 'main.html', {'host_list': host_list})
 
 def add(request):
-    return render(request, 'add.html')
+    if request.method == "POST":
+        form = HostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = HostForm()
+    return render(request, 'add.html', {'form': form})
